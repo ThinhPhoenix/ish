fa3c5c47c3ebddc759b2e60e38bbe27b7b93263b
ThinhPhoenix <laithinh1310@gmail.com>
2025-07-22 15:35:21 +0700

    pretty

--- pretty.sh
+++ pretty.sh
@@ -5,7 +5,7 @@ touch ~/.ashrc
 
 # 2. Ghi cấu hình prompt vào ~/.ashrc
 cat << 'EOF' > ~/.ashrc
-PS1='\n\[\033[37m\]thinhphoenix@iphone\[\033[0m\]\n\[\033[1;34m\]\w\[\033[0m\]\n\[\033[37m\]↝ \[\033[0m\]'
+PS1='\n\[\033[37m\]thinhphoenix@iphone\[\033[0m\]\n\[\033[1;34m\]\w\[\033[0m\]\n\[\033[1;32m\]↝ \[\033[0m\]'
 EOF
 
 # 3. Thêm ENV vào ~/.profile nếu chưa có
